cbb494e211c1f35646f484ad05401fa944e1f1cf
emnafradi55 <emna.fradi@dnext.io>
2026-07-01 16:13:30 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import ProductList from "./components/ProductList";
+
+
+function App() {
+
+  return (
+
+    <div>
+
+      <h1>Products</h1>
+
+      <ProductList />
+
+    </div>
+
+  );
+
+}
+
+
+export default App;
